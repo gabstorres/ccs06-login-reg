03bb4e8c128fde109287f9d60522b01ea5c06667
gabstorres <127963911+gabstorres@users.noreply.github.com>
2023-05-10 04:00:14 +0800

    Update register.php

--- register.php
+++ register.php
@@ -5,176 +5,90 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Registration</title>
 </head>
-<form action="save-registration.php" method="POST" onsubmit="return validateForm();">
+<body>
+<h1>Register a User</h1>
+
+<form action="save-registration.php" method="POST">
 	<div>
-		<label>First Name: </br></label>
-		<input type="text" name="first_name" required/>	
+		<label>First Name</label>
+		<input type="text" name="first_name" placeholder="First Name" />	
 	</div>
 	<div>
-		<label>Middle Name: </br></label>
-		<input type="text" name="middle_name" />
+		<label>Middle Name</label>
+		<input type="text" name="middle_name" placeholder="Middle Name" />	
 	</div>
 	<div>
-		<label>Last Name: </br></label>
-		<input type="text" name="last_name" required />	
+		<label>Last Name</label>
+		<input type="text" name="last_name" placeholder="Last Name" />	
 	</div>
 	<div>
-		<label>Email Address:</br></label>
-		<input type="email" name="email" required />	
-	</div>
-	<label>Password :</br>
-	<input name="password" id="password" type="password" onkeyup='validateForm();' minlength="8" required />
-	</label>
-	<br>
-	<label>Confirm password: </br>
-	<input type="password" name="confirm_password" id="confirm_password" onkeyup='validateForm();' required /> 
-	<span id='message'></span>
-	</label>
-	<br>
-	<div>
-		<label>Birthdate: </br></label>
-		<input type="date" name="birthdate" />	
+		<label>Email Address</label>
+		<input type="email" name="email" placeholder="email@address.com" />	
 	</div>
 	<div>
-		<label>Gender: </br></label>
-		<input type="radio" name="gender" value="Male"/>Male
-		<input type="radio" name="gender" value="Female"/>Female		
+		<label>Password</label>
+		<input type="password" name="password" placeholder="Password" />	
 	</div>
 	<div>
-		<label>Address: </br></label>
-		<input type="text" name="address" />	
+		<label>Confirm Password</label>
+		<input type="password" name="confirm_password" placeholder="Confirm Password" />	
 	</div>
 	<div>
-		<label>Contact Number: </br></label>
-		<input type="tel" name="contact_number" />	
+		<label>Birthdate</label>
+		<input type="date" name="birthdate" placeholder="Birthdate" />	
 	</div>
-	
-	<input type="submit" value="Submit">
+	<div>
+		<label>Gender</label><br />
+		<input type="radio" name="gender" value="Male" />Male<br />	
+		<input type="radio" name="gender" value="Female" />Female<br />	
+	</div>
+	<div>
+		<label>Address</label>
+		<input type="text" name="address" placeholder="Address" />	
+	</div>
+	<div>
+		<label>Contact Number</label>
+		<input type="number" name="contact_number" placeholder="Contact Number" />	
+	</div>
+	<div>
+		<button>
+			Register User
+		</button>	
+	</div>
 </form>
-
 <script>
-	function validateForm() {
-	    var firstName = document.getElementsByName('first_name')[0].value;
-	    var lastName = document.getElementsByName('last_name')[0].value;
-	    var email = document.getElementsByName('email')[0].value;
-	    var password = document.getElementById('password').value;
-	    var confirmPassword = document.getElementById('confirm_password').value;
+function validateForm() {
+    var firstName = document.getElementById("first_name").value;
+    var lastName = document.getElementById("last_name").value;
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+    var confirmPassword = document.getElementById("confirm_password").value;
 
-	    if (firstName === '' || lastName === '') {
-	        document.getElementById('message').style.color = 'red';
-	        document.getElementById('message').innerHTML = 'First name and last name are required';
-	        return false;
-	    } else if (email === '' || password === '') {
-	        document.getElementById('message').style.color = 'red';
-	        document.getElementById('message').innerHTML = 'Email and password are required';
-	        return false;
-	    } else if (password !== confirmPassword) {
-	        document.getElementById('message').style.color = 'red';
-	        document.getElementById('message').innerHTML = 'Passwords do not match';
-	        return false;
-	    } else if (password.length < 8) {
-	        document.getElementById('message').style.color = 'red';
-	        document.getElementById('message').innerHTML = 'Password should be at least 8 characters long';
-	        return false;
-	    } else {
-	        document.getElementById('message').style.color = 'green';
-	        document.getElementById('message').innerHTML = 'Passwords match';
-	        return true;
-	    }
-	};
+    if (firstName == "" || lastName == "") {
+        alert("First and last name are required.");
+        return false;
+    }
 
-	document.getElementById('password').onchange = validateForm;
-	document.getElementById('confirm_password').onkeyup = validateForm;
+    if (email == "" || password == "") {
+        alert("Email address and password are required.");
+        return false;
+    }
+
+    if (password.length < 8) {
+        alert("Password must be at least 8 characters.");
+        return false;
+    }
+
+    if (password != confirmPassword) {
+        alert("Passwords do not match.");
+        return false;
+    }
+
+    return true;
+}
 </script>
 
-</html>
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Registration</title>
-</head>
-<form action="save-registration.php" method="POST" onsubmit="return validateForm();">
-	<div>
-		<label>First Name: </br></label>
-		<input type="text" name="first_name" required/>	
-	</div>
-	<div>
-		<label>Middle Name: </br></label>
-		<input type="text" name="middle_name" />
-	</div>
-	<div>
-		<label>Last Name: </br></label>
-		<input type="text" name="last_name" required />	
-	</div>
-	<div>
-		<label>Email Address:</br></label>
-		<input type="email" name="email" required />	
-	</div>
-	<label>Password :</br>
-	<input name="password" id="password" type="password" onkeyup='validateForm();' minlength="8" required />
-	</label>
-	<br>
-	<label>Confirm password: </br>
-	<input type="password" name="confirm_password" id="confirm_password" onkeyup='validateForm();' required /> 
-	<span id='message'></span>
-	</label>
-	<br>
-	<div>
-		<label>Birthdate: </br></label>
-		<input type="date" name="birthdate" />	
-	</div>
-	<div>
-		<label>Gender: </br></label>
-		<input type="radio" name="gender" value="Male"/>Male
-		<input type="radio" name="gender" value="Female"/>Female		
-	</div>
-	<div>
-		<label>Address: </br></label>
-		<input type="text" name="address" />	
-	</div>
-	<div>
-		<label>Contact Number: </br></label>
-		<input type="tel" name="contact_number" />	
-	</div>
-	
-	<input type="submit" value="Submit">
-</form>
 
-<script>
-	function validateForm() {
-	    var firstName = document.getElementsByName('first_name')[0].value;
-	    var lastName = document.getElementsByName('last_name')[0].value;
-	    var email = document.getElementsByName('email')[0].value;
-	    var password = document.getElementById('password').value;
-	    var confirmPassword = document.getElementById('confirm_password').value;
 
-	    if (firstName === '' || lastName === '') {
-	        document.getElementById('message').style.color = 'red';
-	        document.getElementById('message').innerHTML = 'First name and last name are required';
-	        return false;
-	    } else if (email === '' || password === '') {
-	        document.getElementById('message').style.color = 'red';
-	        document.getElementById('message').innerHTML = 'Email and password are required';
-	        return false;
-	    } else if (password !== confirmPassword) {
-	        document.getElementById('message').style.color = 'red';
-	        document.getElementById('message').innerHTML = 'Passwords do not match';
-	        return false;
-	    } else if (password.length < 8) {
-	        document.getElementById('message').style.color = 'red';
-	        document.getElementById('message').innerHTML = 'Password should be at least 8 characters long';
-	        return false;
-	    } else {
-	        document.getElementById('message').style.color = 'green';
-	        document.getElementById('message').innerHTML = 'Passwords match';
-	        return true;
-	    }
-	};
-
-	document.getElementById('password').onchange = validateForm;
-	document.getElementById('confirm_password').onkeyup = validateForm;
-</script>
-
+</body>
 </html>
